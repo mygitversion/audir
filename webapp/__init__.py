32cc4ddd0b3ecbb8c2b9045773d3892f1804ea23
mygitversion <vn968y@gmail.com>
2022-01-08 21:20:25 +0300

    added DB filling with uploaded files info

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from webapp.model import db 
+from datetime import datetime as dt
+from webapp.model import db, Audir_data 
 from werkzeug.utils import secure_filename
 import os
 
@@ -8,24 +9,57 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    def sort_special(str_of_extentions: str):
+        new_str_of_extentions = ''
+        for item in app.config['ALLOWED_UPLOAD_EXTENSIONS']:
+            if item in str_of_extentions:
+                new_str_of_extentions += ' ' + item
+        return new_str_of_extentions.strip()
+
+
     @app.route('/', methods=["GET","POST"])
     def index():                    
         if request.method == 'POST':
             uploaded_file = request.files['file_name']
             filename = secure_filename(uploaded_file.filename)
-            file_ext = os.path.splitext(filename)[1]
+            file_ext = os.path.splitext(filename)[1][1:]
     
             if filename and (file_ext in app.config['ALLOWED_UPLOAD_EXTENSIONS']):
-                path_to_save = os.path.join(app.config['UPLOAD_PATH'],filename)
-                uploaded_file.save(path_to_save)
-            return redirect(url_for('index'))
+                filename_wo_ext = os.path.splitext(filename)[0]
+                audir_already_in_DB = Audir_data.query.filter(Audir_data.audir_title == filename_wo_ext).first()
+                if audir_already_in_DB:
+                    existing_formats = audir_already_in_DB.available_formats
 
+                if not audir_already_in_DB:
+                    new_audir_for_DB = Audir_data(
+                        audir_title=filename_wo_ext,
+                        uploaded=dt.now(),
+                        available_formats=file_ext)
+
+                    db.session.add(new_audir_for_DB)
+                    db.session.commit()
+                    path_to_save = os.path.join(app.config['UPLOAD_PATH'],filename)
+                    uploaded_file.save(path_to_save)
+                
+                elif (file_ext in ['jpg', 'png']) and \
+                         ('jpg' in existing_formats or 'png' in existing_formats):
+                    return redirect(url_for('index'))
+                
+                elif not (file_ext in existing_formats):
+                    available_formats_edited = file_ext + ' ' + existing_formats
+                    available_formats_sorted = sort_special(available_formats_edited)
+                    audir_already_in_DB.available_formats = available_formats_sorted
+                    db.session.add(audir_already_in_DB)
+                    db.session.commit()
+                    path_to_save = os.path.join(app.config['UPLOAD_PATH'],filename)
+                    uploaded_file.save(path_to_save)
+
+            return redirect(url_for('index'))
         return render_template("index.html")
 
-   
+
     @app.errorhandler(413)
     def too_large(e):
         return "File is too large", 413
-
 
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -4,9 +4,9 @@ db = SQLAlchemy()
 
 class Audir_data(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        audir_title = db.Column(db.String, nullable=False)
+        audir_title = db.Column(db.String, unique=True, nullable=False)
         uploaded = db.Column(db.DateTime, nullable=False)
-        available_formats = db.Column(db.String, unique=True, nullable=False)
+        available_formats = db.Column(db.String, unique=False, nullable=False)
     
         def __repr__(self):
             return '<Audir material: {} {}>'.format(self.audir_title, self.available_formats)
